c645f0f6d38a5b0509ca4c81b276cb63f152596e
Tauqeer Rahim <tauqeer.rahim03@gmail.com>
2026-04-23 12:50:44 +0530

    Initialize express server with middleware, database connection, and route configurations

--- server/index.js
+++ server/index.js
@@ -7,7 +7,13 @@ const userRoutes = require("./routes/userRoutes");
 
 connectDB();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: "https://appitat.netlify.app",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+    }),
+);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use("/uploads", express.static("uploads"));
